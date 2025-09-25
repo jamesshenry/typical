bbb7a1cdc6da3dd03d694a8da94e98ef5e474a30
jamesshenry <79054685+henry-js@users.noreply.github.com>
2025-09-26 00:33:41 +0100

    make sure db is created on install

--- src/Typical/Program.cs
+++ src/Typical/Program.cs
@@ -1,10 +1,34 @@
 ﻿using ConsoleAppFramework;
+using DotNetPathUtils;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualBasic;
 using Typical;
+using Typical.DataAccess;
 using Typical.Services;
 using Velopack;
 
-VelopackApp.Build().Run();
+var pathHelper = new PathEnvironmentHelper(
+    new PathUtilsOptions()
+    {
+        DirectoryNameCase = DirectoryNameCase.CamelCase,
+        PrefixWithPeriod = false,
+    }
+);
+if (OperatingSystem.IsWindows())
+{
+    var appDirectory = Path.GetDirectoryName(AppContext.BaseDirectory)!;
+    VelopackApp
+        .Build()
+        .OnAfterInstallFastCallback(v =>
+        {
+            pathHelper.EnsureDirectoryIsInPath(appDirectory);
+            var dbFile = Path.Combine(appDirectory, "typical.db");
+            Directory.CreateDirectory(LiteDbConstants.DataDirectory);
+            File.Move(dbFile, LiteDbConstants.DbFile);
+        })
+        .OnBeforeUninstallFastCallback(v => pathHelper.RemoveDirectoryFromPath(appDirectory!))
+        .Run();
+}
 
 var services = new ServiceCollection();
 
